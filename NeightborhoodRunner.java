d59d2e90705aa1b03e6d3a1ceb150a19d76b8de7
AD <uiosuiss1@gmail.com>
2025-09-18 13:55:48 -0700

    finished

---
+++ NeightborhoodRunner.java
@@ -0,0 +1,14 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    PainterPlus yogurt = new PainterPlus();
+    frame gurt = new frame();
+    Painting yo = new Painting();
+
+    yogurt.paintGrid();
+    gurt.drawFrame();
+    yo.drawFrame();
+  }
+}
