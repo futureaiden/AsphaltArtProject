6ec73ac60bfe2f21bb1846b2073d4cfbd5410f41
AD <uiosuiss1@gmail.com>
2025-09-22 09:53:24 -0700

    Update NeightborhoodRunner.java

--- NeightborhoodRunner.java
+++ NeightborhoodRunner.java
@@ -2,13 +2,16 @@ import org.code.neighborhood.*;
 
 public class NeighborhoodRunner {
   public static void main(String[] args) {
-
+//There is a painter for each class.
     PainterPlus yogurt = new PainterPlus();
     frame gurt = new frame();
     Painting yo = new Painting();
-
+// Paints the grid red.
     yogurt.paintGrid();
+    // draws the frame in a gold yellow.
     gurt.drawFrame();
+    //draws the mural.
     yo.drawFrame();
   }
+
 }
